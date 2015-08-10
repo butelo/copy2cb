73cafdf4fc813cc2415548ddb4076e949c7f5e3f
Xes García <xes.garcia@mobgen.com>
2015-08-10 17:38:35 +0200

    2nd steps

--- copy2cb.js
+++ copy2cb.js
@@ -4,46 +4,46 @@ app.set('port', process.env.PORT || 3000);
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({
-  defaultLayout: 'main'
+    defaultLayout: 'main'
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+
+var fortune = require('./lib/fortune.js');
 
 
 
-app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('Send to your clipboard');
 
-
-});
-app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send('About send to your clipboard');
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
 
-
-// custom 404 page
-app.use(function(req, res) {
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+app.get('/about', function (req, res) {
+    res.render('about', {
+        fortune: fortune.getFortune()
+    });
 });
 
 
 
-// custom 500 page
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.type('text/plain');
-  res.status(500);
-  res.send('500 - Server Error');
+// 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+// 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 
 
-app.listen(app.get('port'), function() {
-  console.log('Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.');
 });
